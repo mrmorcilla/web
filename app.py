503c1002e16339fe944b070fe09277323e143ec7
Andrés <andromedamorillof@gmail.com>
2025-02-17 11:24:19 -0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/funcion', methods=['GET'])
+def funcion():
+    # Aquí puedes agregar la lógica que quieras
+    return jsonify({"mensaje": "Valor desde Python"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
